26dff61831558a11d790ea600ae8b9daed884036
LauraLD <laura_limadias@hotmail.com>
2017-08-20 18:06:21 -0300

    Add files via upload
    Base para reproduzir experimentos de anotação automática e similaridade entre documentos

---
+++ download.sh
@@ -0,0 +1,264 @@
+get_iplayer --pid p002h45s --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p002mbyq --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p002nrr1 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p002rqss --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p002rqtn --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p002svt6 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p002ysbx --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0033458 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p003544h --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0036wph --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0039spn --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p003c6c1 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p003g44k --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p003jbk1 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p009wjhw --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p003jht1 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p003jzdt --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p003lrtc --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p003md9n --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p003qfx1 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p003qfxm --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p003s77x --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p003v1mg --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p003wx9r --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p003z48k --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0040kgn --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0041845 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004184r --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0045xnp --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0048bjf --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004cc3w --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004d18x --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004gz5b --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004njln --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004j7zg --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004jz9d --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004m63h --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004t1hm --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004t230 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004t2b1 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004t429 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004t4ft --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004t4d8 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004t57h --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004t62h --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004v8xt --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004v8yq --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004vxnz --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004wrdn --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0053d39 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0053dgw --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0053f1f --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0053gbp --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p005dgbv --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p005dgd8 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p004gzq5 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p005ftzw --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p005k307 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p005mzh6 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p005nhvg --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p005qznm --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p005rc0r --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p005x3wj --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p005zyns --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0062ygx --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p00663yr --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0067c2j --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0069chc --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p006dg47 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p006hrrr --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p006lyl2 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p006rbjc --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p006qpsj --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p006x9v0 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p006x9vd --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p006z0ct --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p006ygqy --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p006z0d6 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0071pp2 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p00725n7 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0074wh0 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0075s1d --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p00785tx --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0078p7g --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007czjs --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007gvhq --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007hcby --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007l92p --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007l92y --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007m8vv --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007vj33 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007z15w --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0082kx8 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007qzzk --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007tyj0 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p00833zn --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007yj34 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007qf40 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007tyj8 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007yj3d --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p00821ch --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007qf48 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0089chm --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008gs0c --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0085yx4 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008grq6 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0089cdr --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0088v2l --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008grqh --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0085dyj --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0088v2w --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008jjnj --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008rnrs --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p007k3wy --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008n428 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008n41y --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008nqgm --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008r4l4 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008rntk --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008rnty --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008vd45 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008w7jf --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008z6sl --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008z6sv --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008zr9d --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p008zr9s --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0092pvr --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0092pw0 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p0098ggp --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p009wjhw --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p00b18gk --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
+get_iplayer --pid p00c7r48 --modes=flashaacstd,hlsaaclow --output=/home/ --aactomp3
+
